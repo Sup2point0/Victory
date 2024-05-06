5599c7b4dd45e6714c78c4b53c98f95409d52c17
Sup#2.0 <sup2point0@gmail.com>
2024-05-06 18:11:02 +0100

    develop styles and add health sliding

--- docs/live/core.js
+++ docs/live/core.js
@@ -1,16 +1,35 @@
-// import { Player } from "player";
-// import { processKeyboardShortcuts } from "shortcuts";
+// import { Player } from "player"; //
+// import { processKeyboardShortcuts } from "shortcuts"; //
 
 class Player {
-  static initHealth = 4000;
-  static #shardCount = 0;
+  // STATIC //
+  static healthInit = 4000;
+  static healthDecayRate = 8;
+  static healthDecayThreshold = 10;
 
+  // DYNAMIC //
+  static #shardCount = 0;
+  static activePlayers = [null]; // spare NULL for 1-indexing
+  static healthExceeded = false;
+
+  // FIELDS //
   shard;
   element;
+  meter;
 
   #health;
-  healthDisplayed;
+  healthText;
 
+  // CONSTRUCTOR //
+  constructor(shard = null) {
+    this.shard = shard ?? ++Player.#shardCount;
+    this.element = document.querySelector(`.player-${this.shard} .health-text`);
+    this.meter = document.querySelector(`.player-${this.shard} meter`);
+    this.reset();
+    Player.activePlayers.push(this);
+  }
+
+  // PROPERTIES //
   get health() {
     return this.#health;
   }
@@ -21,22 +40,61 @@ class Player {
       this.#health = 0;
     }
 
-    this.element.innerHTML = this.#health;
-    this.element.style['--health-value'] = this.#health;
+    if (this.#health > Player.healthInit) {
+      Player.healthExceeded = true;
+    }
+
+    if (Player.healthExceeded) {
+      Player.healthExceeded = false;
+   
+      Player.activePlayers.slice(1).forEach(player => {
+        player.meter.max = Math.max(
+          Player.activePlayers[1].health,
+          Player.activePlayers[2].health
+        );
+      });
+    }
+
+    requestAnimationFrame(this._callSlideHealth(this.#health));
   }
 
-  constructor(shard = null) {
-    this.shard = shard ?? ++Player.#shardCount;
-    this.element = document.querySelector(`.player-${this.shard} .health-text`);
-    this.reset();
-  }
-
+  // METHODS //
   reset() {
-    this.health = 4000;
+    this.healthText = Player.healthInit;
+    this.health = Player.healthInit;
+    this.meter.max = Player.healthInit;
   }
 
   halveHealth() {
     this.health -= this.health / 2;
+  }
+
+  static resetAll() {
+    Player.activePlayers.slice(1).forEach(player => player.reset());
+  }
+
+  // INTERNAL //
+  _slideHealth(target) {
+    if (target !== this.#health) return;
+  
+    let delta = this.#health - this.healthText;
+    let final = (Math.abs(delta) < Player.healthDecayThreshold);
+    if (final) {
+      this.healthText = this.#health;
+    } else {
+      this.healthText += delta / Player.healthDecayRate;
+    }
+
+    this.element.innerHTML = Math.round(this.healthText);
+    this.meter.value = Math.round(this.healthText);
+
+    if (!final) {
+      requestAnimationFrame(this._callSlideHealth(target));
+    }
+  }
+  
+  _callSlideHealth(target) {
+    return (timestamp) => this._slideHealth(target);
   }
 }
 
@@ -63,26 +121,36 @@ export function processKeyboardShortcuts(event) {
 
 var player1 = new Player();
 var player2 = new Player();
-// spare NULL for 1-indexing
-var players = [null, player1, player2];
+
+var deltaPolarity = 1;
+var actionStack = [];
 
 
 function updatePlayerHealth(shard) {
-  players[shard].health += parseInt(
-    document.querySelector(
-      `.player-${shard} .health-input input`
-    ).value);
+  let delta = deltaPolarity * parseInt(
+    document.querySelector(`.player-${shard} .health-input input`).value);
+
+  Player.activePlayers[shard].health += delta;
+  actionStack.push({shard: shard, delta: delta,})
 }
 
-document.querySelector(".player-1 .health-input button")
-  .addEventListener("click", event => updatePlayerHealth(1));
-document.querySelector(".player-2 .health-input button")
-  .addEventListener("click", event => updatePlayerHealth(2));
+document.querySelector(".player-1 .health-input input")
+  .addEventListener("keydown", event => {
+    if (event.key == "Enter") updatePlayerHealth(1)} );
+document.querySelector(".player-2 .health-input input")
+  .addEventListener("keydown", event => {
+    if (event.key == "Enter") updatePlayerHealth(2)} );
+
+let swapPolarity = document.getElementById("swap-polarity");
+let swapPolarityChild = swapPolarity.children[0]
+
+swapPolarity.addEventListener("click", event => {
+    deltaPolarity *= -1;
+    swapPolarityChild.innerHTML = deltaPolarity > 0 ? "add" : "remove";
+    swapPolarity.className = "polarity-" + (deltaPolarity > 0 ? "positive" : "negative");
+  });
 
 document.getElementById("action-reset")
-  .addEventListener("click", event => {
-    player1.reset();
-    player2.reset();
-  });
+  .addEventListener("click", event => Player.resetAll());
 
 document.addEventListener("keydown", processKeyboardShortcuts);
