d8643a7cafa7a6cfb783e0af2214e2bf77dcdaaf
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-05-11 10:06:52 +0100

    add `action-history.js`

---
+++ docs/live/action-history.js
@@ -0,0 +1,47 @@
+/*
+Handles tracing actions for undo/redo.
+ */
+
+export class InvertibleAction {
+  action;
+  inverse;
+
+  constructor this(action, arg, inverse = null) {
+    this.action = () => action(arg);
+    this.inverse = inverse ?? (() => action(arg));
+  }
+}
+
+
+export class ActionHistory {
+  #actions = [];
+  cursor = 0;
+  // Use 1-indexing for convenience and convert when needed
+
+  function push(action) {
+    if (this.cursor < this.#actions.length) {
+      this.#actions = this.#actions.slice(0, this.cursor);
+    }
+
+    this.#actions.push(action);
+    this.#cursor = this.#actions.length;
+  }
+
+  function tryUndo() {
+    if (cursor > 0) {
+      this.#actions[cursor--].inverse();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  tryRedo() {
+    if (cursor < this.#actions.length) {
+      this.#actions[cursor++].action();
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
