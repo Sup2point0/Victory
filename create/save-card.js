e4ae70e8445ad471f7bee8634a0937107426174a
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-05-16 09:22:58 +0100

    develop card form submitting script

--- create/save-card.js
+++ create/save-card.js
@@ -1,13 +1,45 @@
-export default function saveCard(data) {
-  return fetch(
-    "https://sup2point0.npkn.net/victory-card-creator",
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }
-  )
-  .catch(error => console.error(error));
+import PopupModal from "./popup-modal.js";
+import popups from "./popup-presets.js";
+
+
+const SAVE_COOLDOWN = 10;
+
+const URL = "https://sup2point0.npkn.net/victory-cards";
+
+
+export default function saveCard(source) {
+  let delta = Date.Now() - saveCards.lastSave;
+  if (delta < SAVE_COOLDOWN) {
+    return popups.Cooldown.show();
+  }
+
+  let field = ($id) => source.querySelector(`input#${$id}`).value;
+  let fields = ($class) => source.querySelectorAll(`input.${$class}`)
+    .map(elem => elem.value
+
+  let data = {
+    id: field("id"),
+    effects: (() => ())(),
+  }
+
+  let request = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }
+
+  let response = fetch(URL, request)
+    .catch(error => popups.Request.show());
+
+  switch response.json().status_code {
+    501 => popups.Server.show();
+  }
 }
+
+saveCard.lastSave = 0;
+
+function findCardData() {}
+function findSpiritData() {}
+function findSpellData() {}
