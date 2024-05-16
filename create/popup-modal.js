a4230263be5ff10b3997ad630e6edb620e87f146
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-05-16 09:42:12 +0100

    add `create/popup-modal.js`

---
+++ create/popup-modal.js
@@ -0,0 +1,20 @@
+export class PopupModal {
+  constructor(type, title, content, buttons = null) {
+    this.html = `<div class="popup ${type.toLowerCase()} active">
+  <div class="popup-box">
+    <h2> ${title} </h2>
+    <p> ${content} </p>
+  </div>
+</div>`;
+
+    this.root = document.querySelector("html .popups");
+  }
+
+  show() {
+    this.root.innerHTML = this.html;
+  }
+
+  hide() {
+    this.root.child.classes = "close";
+  }
+}
